eb9da44d3a180210c7611bc0d662185814f3eda2
gervais <mbadunkokorigervais@gmail.com>
2025-01-27 20:08:28 -0800

    Initial Commit

---
+++ ChangingBackgroundTests/ChangingBackgroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChangingBackgroundTests.swift
+//  ChangingBackgroundTests
+//
+//  Created by Gervais Mbadu on 1/27/25.
+//
+
+import Testing
+@testable import ChangingBackground
+
+struct ChangingBackgroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
